f72f3fa7b0987005661f6488248833e2239614ec
신근재 <shin75492@naver.com>
2022-07-09 18:20:44 +0900

    + 이해 안된 부분 서술

--- 백준/S1_1931_회의실 배정.py
+++ 백준/S1_1931_회의실 배정.py
@@ -5,8 +5,11 @@ for _ in range(N):
     start, end = map(int, input().split())
     ary.append([start, end])
 
+# 이해 어려웠던 부분
+# 종료시간을 기준[빨리 끝나는]으로 정렬하는 건 이해되는데..
+# 왜 시작 시간을 기준으로 정렬할까 => 종료 시간이 똑같을 때는 시작 시간이 빠른 것으로 해야 (2,2) 같은 게 실행 가능!
 ary = sorted(ary, key = lambda a : a[0])
-ary = sorted(ary, key = lambda a : a[1]) # 끝 시간 정렬
+ary = sorted(ary, key = lambda a : a[1])
 
 End_T = 0 
 count = 0
