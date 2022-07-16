e4148e2dc85bc3cc8be043757b7622cfc0966b1e
신근재 <shin75492@naver.com>
2022-07-16 23:54:08 +0900

    S4_1920_수 찾기 [해결]
    220715~16
    
    key in 배열 => 하나씩 다 탐색해서 시간이 오래 걸림으로 이분탐색 방법을 이용하였다.

--- 백준/S4_1920_수 찾기.py
+++ 백준/S4_1920_수 찾기.py
@@ -1,6 +1,23 @@
 import sys
 input=sys.stdin.readline
 
+def Bi_find(key, list):
+    start = 0
+    end = len(list) - 1
+
+    while start <= end :
+        mid = (start + end) // 2  
+        # 찾았다
+        if(list[mid] == key):
+            print('1')
+            return 1
+        # 못찾았다
+        elif(list[mid] < key):
+            start = mid + 1
+        elif(list[mid] > key):
+            end = mid - 1
+    print('0')
+
 i = int(input())
 val1 = list(map(int,input().split()))
 
@@ -8,20 +25,11 @@ k = int(input())
 val2 = list(map(int,input().split()))
 
 val1.sort()
-mid = int(len(val1)/2)
 
-for i in range(k):
-    key = val2[i]
-    while True:
-        if(val1[mid] == key):
-            print('1')
-            break
-        elif(val1[mid] < key):
-            print('up')
-            mid = int((mid + mid*2) / 2)
-        elif(val1[mid] > key):
-            print('down')
-            mid = int(mid/2)
+for i in range(0, len(val2)):
+
+    Bi_find(val2[i], val1)
+
 # k안에 있는 값들 하나씩 꺼내서 i에 있는지 찾아본다.
 
 
