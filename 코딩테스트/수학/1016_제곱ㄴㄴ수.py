9c0fa11153be29ce16f6b2cc1049d3afa9adc701
신근재 <shin75492@naver.com>
2023-02-19 19:08:10 +0900

    1016_제곱 수 [시간초과]

--- 코딩테스트/수학/1016_제곱ㄴㄴ수.py
+++ 코딩테스트/수학/1016_제곱ㄴㄴ수.py
@@ -1,25 +1,26 @@
 # 1000000000000
 mi, ma = map(int, input().split())
-ary = [True for i in range(ma)]
-
-num = 2
+ary = []
 cnt = 0
 
 if(mi == ma):
     print(1)
 else:
     if(ma >= 4):
-        for i in range(2, ma//2, 1):
+        for i in range(2, ma):
+            print(i, ma)
             a = i * i
+            if(a >= ma):
+                print('끝', a, i)
+                break
             value = a
 
+            # 2의 제곱수인 4로 나누어진다면
+            # 16의 제곱수는 할 필요도 없다.
             while(value <= ma):
-                ary[value-1] = False
+                if(not ary.__contains__(value)):
+                    ary.append(value)
                 value += a
 
-    for j in range(mi, ma, 1):
-        if(not ary[j]):
-            cnt += 1
-
     print('--------------')
-    print(ma - cnt)
+    print(ma - len(ary))
