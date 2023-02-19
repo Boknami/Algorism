a96f1a298f723fc09e86171993cb173e41c90a35
신근재 <shin75492@naver.com>
2023-02-19 18:12:31 +0900

    1016_제곱ㄴㄴ수[메모리초과]
    230219

--- 코딩테스트/수학/1016_제곱ㄴㄴ수.py
+++ 코딩테스트/수학/1016_제곱ㄴㄴ수.py
@@ -1,13 +1,25 @@
+# 1000000000000
 mi, ma = map(int, input().split())
+ary = [True for i in range(ma)]
 
-sq = [4]
+num = 2
+cnt = 0
 
-while(max(sq) < ma):
-    num = 2
-    sav = num * num
-    sq.append(sav)
-    num = num + 1
+if(mi == ma):
+    print(1)
+else:
+    if(ma >= 4):
+        for i in range(2, ma//2, 1):
+            a = i * i
+            value = a
+
+            while(value <= ma):
+                ary[value-1] = False
+                value += a
+
+    for j in range(mi, ma, 1):
+        if(not ary[j]):
+            cnt += 1
 
     print('--------------')
-    print(mi, ma)
-    print(num, sq)
+    print(ma - cnt)
