d7cfc2b04f9c04a3b8c9e5a2ccc503a39980bc69
신근재 <60930743+Boknami@users.noreply.github.com>
2023-03-09 23:21:01 +0900

    7576_토마토 (횟수 정답, 특이케이스 추가해야)
    0309
    토마토가 -1에 갇힌 경우에 대비해야한다.

--- 코딩테스트/DFS(BFS)/7576_토마토.py
+++ 코딩테스트/DFS(BFS)/7576_토마토.py
@@ -14,18 +14,24 @@ for i in range(N):
 day = 0
 dx = [0,0,1,-1]
 dy = [1,-1,0,0]
+temp_queue = []
 
-while(queue):
-    cur = queue.pop()
-    
-    # 상하좌우 판단
-    for i in range(4):
-        x = cur[0] + dx[i]
-        y = cur[1] + dy[i]
+# 큐를 2개 만들어서 하나는 소비용, 하나는 다음꺼 저장용
+while(any(0 in l for l in graph)):
+    day += 1
+    while queue:
+        cur = queue.pop()
+        
+        # 상하좌우 판단
+        for i in range(4):
+            x = cur[0] + dx[i]
+            y = cur[1] + dy[i]
 
-        # 크기 안에서 & 0인 값 & 큐에 없는 값
-        if( (0<= x <= 3 and 0<= y<= 5) and graph[x][y] == 0 and [x,y] not in queue):
-            queue.append([x,y])
-            graph[x][y] = 1
-            
+            # 크기 안에서 & 0인 값 & 큐에 없는 값
+            if( (0<= x <= N-1 and 0<= y<= M-1) and graph[x][y] == 0 and [x,y] not in queue):
+                temp_queue.append([x,y])
+                graph[x][y] = 1
+
+    queue = temp_queue[:]
+
 print(graph, day)
