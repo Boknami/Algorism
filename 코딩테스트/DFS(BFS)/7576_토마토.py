31893a1bbeef970ad0195dd8677e231f24f0e761
신근재 <60930743+Boknami@users.noreply.github.com>
2023-03-10 16:18:43 +0900

    7576_토마토 (해결)
    0310

--- 코딩테스트/DFS(BFS)/7576_토마토.py
+++ 코딩테스트/DFS(BFS)/7576_토마토.py
@@ -1,6 +1,12 @@
-graph = []
-queue = []
+from collections import deque
+import sys
+input=sys.stdin.readline
 
+dx = [0,0,1,-1]
+dy = [1,-1,0,0]
+graph = []
+day = 0
+q = deque([])
 M,N = map(int, input().split())
 
 for i in range(N):
@@ -9,29 +15,26 @@ for i in range(N):
 for i in range(N):
     for j in range(M):
         if(graph[i][j] == 1):
-            queue.append([i,j])
+            q.append([i,j])
 
-day = 0
-dx = [0,0,1,-1]
-dy = [1,-1,0,0]
-temp_queue = []
+while q:
+    cur = q.popleft()
 
-# 큐를 2개 만들어서 하나는 소비용, 하나는 다음꺼 저장용
-while(any(0 in l for l in graph)):
-    day += 1
-    while queue:
-        cur = queue.pop()
-        
-        # 상하좌우 판단
-        for i in range(4):
-            x = cur[0] + dx[i]
-            y = cur[1] + dy[i]
+    # 상하좌우 판단
+    for i in range(4):
+        x = cur[0] + dx[i]
+        y = cur[1] + dy[i]
 
-            # 크기 안에서 & 0인 값 & 큐에 없는 값
-            if( (0<= x <= N-1 and 0<= y<= M-1) and graph[x][y] == 0 and [x,y] not in queue):
-                temp_queue.append([x,y])
-                graph[x][y] = 1
+        # 크기 안에서 & 0인 값 & 큐에 없는 값
+        if( (0<= x <= N-1 and 0<= y<= M-1) and graph[x][y] == 0):
+            q.append([x,y])
+            graph[x][y] = graph[cur[0]][cur[1]] + 1
+            print(q)
 
-    queue = temp_queue[:]
-
-print(graph, day)
+for i in (graph):
+    for j in i:
+        if(day < j) : day = j
+        if(j == 0):
+            print(-1)
+            exit(0)
+print(day - 1)
