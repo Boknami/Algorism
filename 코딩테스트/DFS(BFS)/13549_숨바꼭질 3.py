aa91107075034a89d384fe33f19b9cb3d2a13c22
신근재 <shin75492@naver.com>
2023-03-12 16:55:23 +0900

    13549_숨바꼭질 3 (해결)
    0312

--- 코딩테스트/DFS(BFS)/13549_숨바꼭질 3.py
+++ 코딩테스트/DFS(BFS)/13549_숨바꼭질 3.py
@@ -2,30 +2,30 @@ import sys
 from collections import deque
 input=sys.stdin.readline
 
+MAX = 1000001
 def bfs():
     q = deque()
-    q.append([start,0])
-    visited = [0] * 10000000 #방문여부체크 배열
+    q.append(start)
+    visited = [0] * MAX #방문여부체크 배열
+    visited[start] = 1
 
     while(True):
-        qq = q.popleft()
-        q_value = qq[0] #큐에서 뽑은 값
-        q_cnt = qq[1] #큐에 횟수
+        q_value = q.popleft()
 
+        if(q_value == target):
+            print(visited[q_value] -1 )
+            return
+        
         # 큐 뽑은 거 연산
         for select in (q_value-1, q_value+1, q_value*2):
-            # 종료조건
-            if(select == target):
-                return q_cnt+1
-            
             # 방문여부 확인 후 큐에 넣자
-            if(select <= 1000000 and visited[select] == 0):
-                visited[select] = 1
+            if(0 <= select <= MAX-1 and visited[select] == 0):
+                q.append(select)
 
                 if(select == q_value*2):
-                    q.append([select,q_cnt])
+                    visited[select] = visited[q_value]
                 else:
-                    q.append([select,q_cnt+1])
+                    visited[select] = visited[q_value] + 1
                 
 start, target = map(int, input().split())
-print(bfs())
+bfs()
