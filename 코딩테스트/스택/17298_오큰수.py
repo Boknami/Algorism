95f6913cf3026ef148f10f25d48f2461f15069b3
신근재 <60930743+Boknami@users.noreply.github.com>
2023-03-13 23:00:20 +0900

    17298_오큰수 (아이디어 생각중)

--- 코딩테스트/스택/17298_오큰수.py
+++ 코딩테스트/스택/17298_오큰수.py
@@ -1,26 +1,22 @@
+import sys
+from collections import deque
+input=sys.stdin.readline
+
 n = int(input())
-stack = list(map(int,input().split()))
+dq = list(map(int,input().split()))
 
-for i in range(n):
-    temp_s = stack[:]
+# 스택을 이용
+# 오른쪽에서부터 숫자를 꺼내는 아이디어
+# 타 배열에 꺼낸 걸 저장
+# 진행 : 반복 (끝보다 1칸 앞 ~ 0까지) 팝하고 appendleft()
+# 현재 꺼낸 것과 배열에 첫 부분부터 검사
 
-    # 해당 번째 인덱스를 팝
-    value = temp_s.pop(i)
-    # while문으로 해당 번째 인덱스 팝하고
-    while(temp_s):
-        if(i == n-1):
-            print('-1')
-            break
-
-        compare_v = temp_s.pop(i)
-
-        #print(value,':',compare_v)
-        if(value < compare_v):
-            print(compare_v, end=' ')
-            break
-
-        # 못 찾고 종료
-        if(len(temp_s) == i):
-            print('-1', end=' ')
-            break
+save_ary = [dq.pop()]
+while(dq):
+    cur = dq.pop()
     
+    for i in save_ary:
+        if(cur < i):
+            print(cur)
+            break
+    dq.append(cur)
