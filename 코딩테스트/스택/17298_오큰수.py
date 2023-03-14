944c54ed71643db9988974b1076602d311af481d
신근재 <60930743+Boknami@users.noreply.github.com>
2023-03-14 23:52:18 +0900

    17298_오큰수 (개선)

--- 코딩테스트/스택/17298_오큰수.py
+++ 코딩테스트/스택/17298_오큰수.py
@@ -5,22 +5,26 @@ input=sys.stdin.readline
 n = int(input())
 dq = deque(map(int,input().split()))
 
-# 스택을 이용
-# 오른쪽에서부터 숫자를 꺼내는 아이디어
-# 타 배열에 꺼낸 걸 저장
-# 진행 : 반복 (끝보다 1칸 앞 ~ 0까지) 팝하고 appendleft()
-# 현재 꺼낸 것과 배열에 첫 부분부터 검사
-
 save=[]
+list_max = max(dq)
+
 while(dq):
     cnt=1
     cur = dq.popleft()
 
+    # cur보다 큰 수가 존재하는 경우 while에 들어가게..
+    if(cur == list_max or len(dq)== 0):
+        save.append(-1)
+        continue
+
     while(True):
-        comp =dq.popleft()
-        if (cur< comp):
+        comp = dq.popleft()
+        if (cur < comp):
+            dq.appendleft(comp)
             break
         cnt+=1
     for i in range(cnt):
         save.append(comp)
-print(save)
+
+for i in save:
+    print(i, end= ' ')
