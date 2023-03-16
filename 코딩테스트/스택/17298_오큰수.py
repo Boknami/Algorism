c40ded3e78932f7a6a291676aaa75cdab776392a
신근재 <60930743+Boknami@users.noreply.github.com>
2023-03-16 23:02:59 +0900

    1181_단어정렬 (생성)
    두통이 너무 심해서 따로 작업 없이 바로 파일만 올린다.

--- 코딩테스트/스택/17298_오큰수.py
+++ 코딩테스트/스택/17298_오큰수.py
@@ -6,23 +6,32 @@ n = int(input())
 dq = deque(map(int,input().split()))
 
 save=[]
-list_max = max(dq)
+
 
 while(dq):
     cnt=1
+    list_max = max(dq)
     cur = dq.popleft()
 
-    # cur보다 큰 수가 존재하는 경우 while에 들어가게..
-    if(cur == list_max or len(dq)== 0):
+    # 현재가 가장 큰 경우 or 더 이상 팝할게 없으면
+    if(cur == list_max or len(dq) == 0):
         save.append(-1)
         continue
-
-    while(True):
+    
+    # 반복 : 큐가 남아있는 동안
+    while(dq):
+        # 비교 값 pop
         comp = dq.popleft()
+
+        # 현재 값보다 더 큰 값 존재
         if (cur < comp):
-            dq.appendleft(comp)
+            dq.appendleft(comp) # 빼낸 거 다시 넣고
             break
+            
+        # 넘어간 비교숫자 갯수 기록
         cnt+=1
+    
+    # cnt만큼 배열에 기록한다.
     for i in range(cnt):
         save.append(comp)
 
