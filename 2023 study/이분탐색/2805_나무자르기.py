d8265699fb8cb9d40c7acf69e8754fc4441af2b7
신근재 <60930743+Boknami@users.noreply.github.com>
2023-03-22 15:25:17 +0900

    2805_나무자르기 (해결)
    0322

--- 2023 study/이분탐색/2805_나무자르기.py
+++ 2023 study/이분탐색/2805_나무자르기.py
@@ -1,13 +1,28 @@
-# 나무 M 미터
-# 이만큼 짤라라 높이 설정 H
-# 20 10 15 (H = 10) -> 10 + 0 + 5 = 15M 가져가
-# 최소 M 미터는 가져갈 높이 H의 최댓값
-
-# 나무 최대 100만, 길이 최대20억 
 import sys
 input = sys.stdin.readline
 
 cntWood, needLength = map(int, input().split())
 woods = list(map(int, input().split()))
 
-curLength = 0
+# 이분탐색으로 길이를 '유추'하면서 맞춰보자
+end = max(woods)
+start = 0
+
+# while로 원하는 숫자만큼 짜를 때까지 반복
+while(start <= end):
+    mid = (end + start)//2
+    sum = 0
+
+    # 나무들 중 현재mid보다 커서 1cm라도 짜를 수 있다면 짤라가보자
+    for wood in woods:
+        if(mid < wood):
+            sum += wood-mid
+
+    if(sum == needLength):
+        print(mid)
+        exit(0)
+    elif(sum > needLength):
+        start = mid + 1
+    else:#더 필요하다
+        end = mid - 1
+print(end)
