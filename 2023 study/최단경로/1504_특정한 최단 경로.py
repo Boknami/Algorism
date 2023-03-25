f8554f90aac9a382dd59bfc37d065c2696749a66
신근재 <60930743+Boknami@users.noreply.github.com>
2023-03-25 16:12:52 +0900

    1504_특정한 최단 경로(해결!)
    0325

--- 2023 study/최단경로/1504_특정한 최단 경로.py
+++ 2023 study/최단경로/1504_특정한 최단 경로.py
@@ -2,10 +2,10 @@ import sys
 import heapq
 input = sys.stdin.readline
 
-def dijkstra(start, target):
+def dijkstra(start):
     q = []
-    costLeast = [1e9 for i in range(cntNode + 1)]# 최소비용 배열
-    costLeast[1] = 1
+    costLeast = [ 1e9 for i in range(cntNode + 1)]# 최소비용 배열
+    costLeast[start] = 0
     # 큐에 (비용,시작)
     heapq.heappush(q, (start, 0))
 
@@ -19,20 +19,20 @@ def dijkstra(start, target):
                 heapq.heappush(q, (targetNode, allCost))
     return costLeast
 
-
 cntNode, cntLine = map(int,input().split())
 graph = [[] for _ in range(cntNode + 1) ]
 
 for _ in range(cntLine):
     start, end, cost = map(int, input().split())
     graph[start].append((end, cost))
+    graph[end].append((start, cost))
 
 mustGo1, mustGo2 = list(map(int,input().split()))
-print(dijkstra(1,mustGo1))
-print(dijkstra(2,mustGo2))
-print(dijkstra(3,mustGo2))
+cost_start = dijkstra(1)
+cost_mustgo1 = dijkstra(mustGo1)
+cost_mustgo2 = dijkstra(mustGo2)
+road1 = cost_start[mustGo1] + cost_mustgo1[mustGo2] + cost_mustgo2[cntNode]
+road2 = cost_start[mustGo2] + cost_mustgo2[mustGo1] + cost_mustgo1[cntNode]
 
-
-# 단순 최소 경로가 아니라 꼭 들려야한다..
-# 다익스트라(출발, 꼭 방문1) -> 다익스트라(출발, 꼭 방문2) -> 최종노드
-# 다익스트라(출발, 꼭 방문2) -> 다익스트라(출발, 꼭 방문1) -> 최종노드
+leastCost = min(road1, road2)
+print(leastCost if leastCost < 1e9 else -1)
