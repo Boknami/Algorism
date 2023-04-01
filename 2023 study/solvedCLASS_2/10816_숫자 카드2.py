34846031cd62a24bb5e5a13eabb40bf87d17fc82
신근재 <60930743+Boknami@users.noreply.github.com>
2023-04-02 00:15:03 +0900

    숫자 카드2 (해결)

--- 2023 study/solvedCLASS_2/10816_숫자 카드2.py
+++ 2023 study/solvedCLASS_2/10816_숫자 카드2.py
@@ -3,5 +3,16 @@ nList = list(map(int, input().split()))
 m = int(input())
 listCheck = list(map(int, input().split()))
 
-for idx in listCheck:
-    print(nList.count(idx), end=" ")    
+dict = {}
+
+for value in nList:
+    if(value in dict):
+        dict[value] += 1
+    else:
+        dict[value] = 1 
+
+for check in listCheck:
+    if(check in dict):
+        print(dict[check], end=" ")
+    else:
+        print(0, end=" ")    
